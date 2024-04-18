62f7cd17597504f09f201859307372b8e6919a9d
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2024-04-18 06:38:21 +0200

    Update menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -200,7 +200,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     conn.reply(m.chat, tradutor.texto2, m);
   }
 };
-handler.command = /^(مهام|قائمه|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|keyaudio|keyaudios)$/i;
+handler.command = /^(مهام|قائمه|اوامر|الاوامر|أوامر|ألاوامر|menu|memuaudio|audios|keyaudio|keyaudios)$/i;
 handler.exp = 50;
 handler.fail = null;
 export default handler;
