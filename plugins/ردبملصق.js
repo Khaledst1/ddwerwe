1dccfaebade17d3b131c39fee8aeddebe041257d
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2024-04-19 01:36:37 +0200

    Update ردبملصق.js

--- plugins/ردبملصق.js
+++ plugins/ردبملصق.js
@@ -4,8 +4,8 @@ let handler = async (m, { conn }) => {
     // إذا لم يكن هناك sticker وكان المحادثة في مجموعة، قم بإلقاء استثناء
     if (!db.data.chats[m.chat].stickers && m.isGroup) throw 0
  
-    let nombre = '201276638909'
-    let nombre2 = '201276638909'
+    let nombre = '201277272498'
+    let nombre2 = '201277272498'
  
     const s = [
         'https://telegra.ph/file/581e6160af36def7872da.png',
@@ -21,14 +21,14 @@ let handler = async (m, { conn }) => {
     await delay(5 * 5000)
     
     // تعريف المتغير nn بالكلام "يوهان"
-    let nn = 'زياد';
+    let nn = 'بوت';
 
     // استخدام المتغير nn داخل الكود
     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `h`, mediaType: 2, sourceUrl: nn, thumbnail: imagen1}}}, { quoted: m })
 }
 
 // يحدد الكود كيفية استجابة الروبوت على الرسائل التي تبدأ بكلمة يوهان
-handler.customPrefix = /^زياد|زيزو|زوز$/i 
+handler.customPrefix = /^يوسف|سلطان|السلطان$/i 
 handler.command = new RegExp
 handler.exp = 50
 export default handler
