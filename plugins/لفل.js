e92d1a8ab6c89569eef9e5682c3fa81ba3673cad
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2024-04-22 07:34:52 +0200

    Update لفل.js

--- plugins/لفل.js
+++ plugins/لفل.js
@@ -1,99 +1,37 @@
 import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
 
 let handler = async (m, { conn }) => {
-let { role } = global.db.data.users[m.sender]
-let name = conn.getName(m.sender)
- let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/moyt.jpg')
-let user = global.db.data.users[m.sender]
-    if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-        let { min, xp, max } = xpRange(user.level, global.multiplier)
-      let lvl = `┓━━━━【 *الـتصـنـيف* 】━━━━┏
-┇ *☎️ الاسم* : ${name} ღ
-┇ *🚒 الفل :* *${user.level}*
-┇ *♟️ مصنف :* ${role}
-┇ *♨️ نقاط الخبرة :* *${user.exp - min}/${xp}*
-┛━━━⊰ بـــــــوت ⊱━━━┗
-
-*تحتاج ${max - user.exp} من نقاط الخبرة للوصول الي مستوي جديد*`
-conn.sendFile(m.chat, pp, 'levelup.jpg', lvl, m)
-    }
-
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-    if (before !== user.level) {
-        let teks = `عاش يحب! ${conn.getName(m.sender)} المستوي: ${user.level}`
-        let str = `┓━━━━【 *الـتصـنـيف* 】━━━━┏
-┇♨️ *المستوي السابق :* *${before}*
-┇🎉 *المستوي الحالي :* *${user.level}*
-┇♟️ *التصنيف :* ${role} 
-┛━━━⊰ بـــــوت ⊱━━━┗`.trim()
-        try {
-            const img = await levelup(teks, user.level)
-            conn.sendFile(m.chat, pp, 'levelup.jpg', str, m)
-        } catch (e) {
-            m.reply(str)
-        }
-    }
-await delay(5 * 5000)  
-}
-handler.help = ['levelup']
-handler.tags = ['xp']
-
-handler.command = ['nivel', 'lvl', 'رانك', 'لفل'] 
-
-export default handler
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
-/*import { canLevelUp, xpRange } from '../lib/levelling.js'
-let handler = async (m, { conn }) => {
-    let name = conn.getName(m.sender)
-   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/killua.jpg')
+	let name = conn.getName(m.sender)
     let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
-        let txt = `
-┓━━【 *الـتصـنـيف* 】━━┏
-☎️ الـرقـم : *${name}*
-🚒 الـلـفـل : *${user.level}*
-♨️ الـاكـس بـي : *${user.exp - min}/${xp}*
-♟️ الـتـصـنيـف : *${user.role}*
+        throw `
+┌───⊷ *المستوي*
+▢ الاسم : *${name}*
+▢ المستوي : *${user.level}*
+▢  الدور : *${user.role}*
+XP : *${user.exp - min}/${xp}*
+└──────────────
 
-ناقـصـك *${max - user.exp}* من *الـاكس  بـي* لـلـصعود الـى لفـل جـديـد
+انت تحتاج الي *${max - user.exp}* *XP* لرفع مستواك
 `.trim()
-try {
-  let imgg = API('fgmods', '/api/rank', {
-    username: name,
-    xp: user.exp - min,
-    exp: xp,
-    avatar: pp,
-    level: user.level,
-    background: 'https://i.ibb.co/CsNgBYw/qiyana.jpg'
-}, 'apikey')
-
-    conn.sendFile(m.chat, pp, 'level.jpg', txt, m)
-} catch (e) {
-    m.reply(txt)
-}
     }
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
-      user.role = global.rpg.role(user.level).name
-
+        let teks = `🎊 عاش يحب ${conn.getName(m.sender)}    المستوي:`
         let str = `
-┓━━【 *لفل جديد* 】━━┏
-♨️ اللفل القديم : *${before}*
-🎉 اللفل الجديد : *${user.level}*
-♟️ التصنيف : *${user.role}*
+┌─⊷ *المستوي*
+▢ المستوي السابق : *${before}*
+▢ المستوي الحالي : *${user.level}*
+└──────────────
+
+*_كلما تفاعلت مع البوت ارتفع مستواك_*
 `.trim()
-         try {
-            let img = API('fgmods', '/api/levelup', { 
-                avatar: pp 
-             }, 'apikey')
-      conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+        try {
+            const img = await levelup(teks, user.level)
+            conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
         } catch (e) {
             m.reply(str)
         }
@@ -101,7 +39,8 @@ try {
 }
 
 handler.help = ['levelup']
-handler.tags = ['econ']
-handler.command = ['رانك', 'لفل', 'levelup', 'lvl'] 
+handler.tags = ['xp']
 
-export default handler*/
+handler.command = ['لفل', 'lvl', 'levelup', 'مستواي', 'رانك'] 
+
+export default handler
