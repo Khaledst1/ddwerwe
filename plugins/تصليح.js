bb52616cf43ac286662bad613a77fe46b8daf4c1
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2024-04-22 07:08:48 +0200

    Update تصليح.js

--- plugins/تصليح.js
+++ plugins/تصليح.js
@@ -1,14 +1,12 @@
-/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
-
 import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
 import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: '*[❗] استخدم هذا الأمر مباشرة في العدد الرئيسي من الروبوت*'}, {quoted: m});
+    return conn.sendMessage(m.chat, {text: '*[❗] استخدم هذا الأمر مباشرة في الرقم الرئيسي للبوت*'}, {quoted: m});
   }
   const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
-  const sessionPath = 'MyvenomSession';
+  const sessionPath = './MysticSession/';
   try {
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -22,15 +20,22 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
     if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: '*[❗] لم يتم العثور على ملف يتضمن معرف الدردشة*'}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: '*[❗] لم يتم العثور على أي ملف يتضمن معرف الدردشة*'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `*[❗] تم حذف ${filesDeleted} ملف من الجلسه بتاعه البوت*`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `*[❗] تمت ازالة ${filesDeleted} ملفات من الجلسة*`}, {quoted: m});
     }
   } catch (err) {
-    console.error('خطأ في قراءة المجلد أو ملفات الجلسة:', err);
-    await conn.sendMessage(m.chat, {text: '*[❗] حدث خطأ عند إزالة ملفات الجلسة*'}, {quoted: m});
+    console.error('Error al leer la carpeta o los archivos de sesión:', err);
+    await conn.sendMessage(m.chat, {text: '*[❗] حدث خطأ في إزالة أرشيفات الجلسة*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `*👋 ¡Hola! Ahora me ves?*\n\n*[❗] اذا لم يرد البوت علي الامر الخاص بك يرجي وضع مثال صغير 3 مرات*\n\n*—◉ مثال:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `*👋 *👋 ¡مرحبا! ياصديقي?*
+
+*[❗] إذا لم يقم البوت بالرد على أوامرك بسبب التشفير او في انتظار الرسالة فيجب كتابة لأمر ادناه*
+
+*—◉ المثال:*
+.ds
+.ds
+.ds*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
 };
 handler.help = ['fixmsgespera'];
 handler.tags = ['fix'];
