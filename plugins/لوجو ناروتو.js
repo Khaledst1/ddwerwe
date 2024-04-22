dc54f77d17e06c9f18af803231f69e6b5236114f
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2024-04-22 07:13:40 +0200

    Update لوجو ناروتو.js

--- plugins/لوجو ناروتو.js
+++ plugins/لوجو ناروتو.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  let too = `[❗] اعمل لوجو ناروتو\n\n *مـثــال* :\n*${usedPrefix + command}* ZORO`
+  let too = `[❗] اعمل لوجو ناروتو\n\n *مـثــال* :\n*${usedPrefix + command}* YOUSSEF`
 
   if (!text) throw too
   let lr = (`https://shizoapi.onrender.com/api/photooxy/naruto?text=${encodeURIComponent(text)}&apikey=shizo`)
   conn.sendFile(m.chat, lr, 'Zoro.png', `تم بواسطه ✅
-  𝑍𝑂𝑅𝑂⚡3𝑀𝐾`, m)
+  بوت السلطان⚡عمك`, m)
 }
 handler.help = ['Zoro']
 handler.tags = ['Zoro']
