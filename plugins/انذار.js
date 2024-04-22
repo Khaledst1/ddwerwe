e2d600f125d26ffeff5faa06aedc5d818c9cc077
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2024-04-22 09:18:48 +0200

    Update انذار.js

--- plugins/انذار.js
+++ plugins/انذار.js
@@ -1,56 +1,26 @@
-const handler = async (m, {conn, text, command, usedPrefix}) => {
-  if (m.mentionedJid.includes(conn.user.jid)) return;
-  const pp = './src/warn.jpg';
-  let who;
-  if (m.isGroup) {
-    who = m.mentionedJid[0] ?
-      m.mentionedJid[0] :
-      m.quoted ?
-      m.quoted.sender :
-      text;
-  } else who = m.chat;
-  const user = global.db.data.users[who];
-  const bot = global.db.data.settings[conn.user.jid] || {};
-  const dReason = 'بدون سبب';
-  const msgtext = text || dReason;
-  const sdms = msgtext.replace(/@\d+-?\d* /g, '');
-  const warntext = `*[❗] قم بالرد علي رساله او منشن المستخدم*\n\n*—◉ مثال:*\n*${
-    usedPrefix + command
-  } @${global.suittag}*`;
-  if (!who) {
-    throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
-  }
-  user.warn += 1;
-  await m.reply(
-      `${
-      user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`
-      } لقد تلقيت تحذيرا في الجروب!\nالسبب: ${sdms}\n*التحذيرات ${
-        user.warn
-      }/3*`,
-      null,
-      {mentions: [who]},
-  );
-  if (user.warn >= 3) {
-    if (!bot.restrict) {
-      return m.reply(
-          '*[❗𝐈𝐍𝐅𝐎❗] المالك مفعلش الطرد كلمه عشان يفعلها*',
-      );
-    }
-    user.warn = 0;
-    await m.reply(
-        `حذرتك عدده مرات!!\n*@${
-          who.split`@`[0]
-        }* لقد تجاوزت *3* التحذيرات، الان سيتم القداء عليك/ي 👽`,
-        null,
-        {mentions: [who]},
-    );
-    await conn.groupParticipantsUpdate(m.chat, [who], 'remove');
-  }
-  return !1;
-};
-
-handler.command = /^(advertir|advertencia|تحذير|انذار)$/i;
-handler.group = true;
-handler.owner = true;
-handler.botAdmin = true;
-export default handler;
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+let pp = './src/warn.jpg'
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+else who = m.chat
+let user = global.db.data.users[who]
+let bot = global.db.data.settings[conn.user.jid] || {}
+let warntext = `*[❗] يرجي وضع منشن للشخص او الرد علي رساله له @منشن*\n\n*—◉ مثال:*\n*${usedPrefix + command} @${global.suittag}*`
+if (!who) throw m.reply(warntext, m.chat, { mentions: conn.parseMention(warntext)}) 
+user.warn += 1
+await conn.sendButton(m.chat,`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} 𝚁حصلت على تحذير في هذه المجموعة!`, `*تحذيرات ${user.warn}/3*\n\n${wm}`, pp, [['📋 قائمة التحذيرات 📋', '#listwarn']], m, { mentions: [who] })
+	
+if (user.warn >= 3) {
+if (!bot.restrict) return m.reply('*[❗] لم يتم تمكين القيود على مالك الروبوت (#تعفيل) استخدم هذا الامر لتمكين التحذيرات*')        
+user.warn = 0
+await m.reply(`لقد حذرتك عدة مرات!!\n*@${who.split`@`[0]}* حتي اصبحوا *3* تحذيرات سوف يتم طردك بعد 0 ثانية 👽`, null, { mentions: [who]})
+//user.banned = true
+await conn.groupParticipantsUpdate(m.chat, [who], 'remove') 
+} 
+return !1
+}
+handler.command = /^(advertir|انذار|warn|تحذير)$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+export default handler
