4c86c7728ce420738b60abaf4c037507488b84e4
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2024-04-23 14:26:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -384,7 +384,7 @@ global.reloadHandler = async function(restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate);
   }
 
-  conn.welcome = '*⦓          ⦓😶‍🌫️⦔          ⦔*\n*❤️ꪶ→ @subject*\n*🍀ꪶ→ @user*\n*😍ꪶ→مـرحـبـاً بـك فـي الــجروب نـورت\n*💠ꪶ→ اقـرا الوصف*\n*⦓          ⦓😶‍🌫️⦔          ⦔*\n *المجموعه:*\n\n${groupMetadata.subject}\n\n*⦓          ⦓😶‍🌫️⦔          ⦔*';
+  conn.welcome = '*⦓          ⦓😶‍🌫️⦔          ⦔*\n*❤️ꪶ→ @subject*\n*🍀ꪶ→ @user*\n*😍ꪶ→مـرحـبـاً بـك فـي الــجروب نـورت\n*💠ꪶ→ اقـرا الوصف*\n*⦓          ⦓😶‍🌫️⦔          ⦔*\n *المجموعه:*\n\n@subject\n\n*⦓          ⦓😶‍🌫️⦔          ⦔*';
   conn.bye = '*⦓          ⦓👋🏻⦔          ⦔*\n*😏ꪶ→ @user*\n*ꪶ→ لا بأس لم يكن له وجود اساسا 🤣*\n*⦓          ⦓👋🏻⦔          ⦔*';
   conn.spromote = '*[ 📌 ] @user تمت ترقيته إلى المسؤول.*';
   conn.sdemote = '*[ 📌 ] @user تم تخفيض رتبته من المسؤول.*';
