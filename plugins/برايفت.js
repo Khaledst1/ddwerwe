d87ae8bb02ee919e6083d342c6d0db48c5e682fb
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2024-04-24 23:53:50 +0200

    Update برايفت.js

--- plugins/برايفت.js
+++ plugins/برايفت.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, *https://whatsapp.com/channel/0029VaL2bnW0rGiPZq8B5S2M كلم المطور لرفع لحظر عنكك او ادخل جروب دعم رسمي للبوت المطور يمنع التحدث بالرسائل الخاصة للبوت وبالتالي سيتم حظرك.*`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, *https://whatsapp.com/channel/0029VaL2bnW0rGiPZq8B5S2M كلم المطور لرفع لحظر عنكك او ادخل جروب دعم رسمي للبوت المطور يمنع التحدث بالرسائل الخاصة للبوت وبالتالي سيتم حظرك.*`, true, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
