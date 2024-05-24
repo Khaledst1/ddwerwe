b71fb0be7f3752e5aa36fba4c76b989b449f456c
Khaledst1 <166001727+Khaledst1@users.noreply.github.com>
2024-05-24 15:25:09 +0300

    Update menu-audios (2).js

--- plugins/menu-audios (2).js
+++ plugins/menu-audios (2).js
@@ -1,34 +1,30 @@
-import fetch from  node-fetch ;
-
-
-
-
-const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.menu_audios
-
-  try {
-    const pp = imagen4;
-    // let vn =  ./media/menu.mp3 
-    const img =  ./Menu2.jpg ;
-    const d = new Date(new Date + 3600000);
-    const locale =  ar ;
-    const week = d.toLocaleDateString(locale, {weekday:  long });
-    const date = d.toLocaleDateString(locale, {day:  numeric , month:  long , year:  numeric });
-    const _uptime = process.uptime() * 1000;
-    const uptime = clockString(_uptime);
-    const user = global.db.data.users[m.sender];
-    const {money, joincount} = global.db.data.users[m.sender];
-    const {exp, limit, level, role} = global.db.data.users[m.sender];
-    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
-    const more = String.fromCharCode(8206);
-    const readMore = more.repeat(850);
-    const taguser =  @  + m.sender.split( @s.whatsapp.net )[0];
-    const doc = [ pdf ,  zip ,  vnd.openxmlformats-officedocument.presentationml.presentation ,  vnd.openxmlformats-officedocument.spreadsheetml.sheet ,  vnd.openxmlformats-officedocument.wordprocessingml.document ];
-    const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `
+import fs from  fs 
+import fetch from  node-fetch 
+import { xpRange } from  ../lib/levelling.js 
+const { levelling } =  ../lib/levelling.js 
+import PhoneNumber from  awesome-phonenumber 
+import { promises } from  fs 
+import { join } from  path 
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
+try {
+let vn =  ./Menu2.jpg ;
+let pp = imagen4
+let img = await(await fetch( https://telegra.ph/file/adaea082937be337d4543.jpg. )).buffer()
+let d = new Date(new Date + 3600000)
+let locale =  ar 
+let week = d.toLocaleDateString(locale, { weekday:  long  })
+let date = d.toLocaleDateString(locale, { day:  numeric , month:  long , year:  numeric  })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let user = global.db.data.users[m.sender]
+let { money, joincount } = global.db.data.users[m.sender]
+let { exp, limit, level, role } = global.db.data.users[m.sender]
+let { min, xp, max } = xpRange(level, global.multiplier)
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser =  @  + m.sender.split("@s.whatsapp.net")[0]
+let str = `
 > *⟣┈اهلا, ${taguser}*
 
 > *⟣┈📆تاريخ:* ${date}
@@ -36,7 +32,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 > *⟣┈رقم اصدار البوت: v1*
 > *⟣┈اسم البوت , بوت السلطان*
 > *⟣┈حط قبل كل امر  ( . )*
-> *⟣┈اســم الـمطور : يوسف السلطان*
+> *⟣┈اســم الـمطورين : يوسف السلطان&ابوهايف:♡*
 > *⟣┈اليك القائمه يحب*  ${taguser}
 > *⟣┈هذا البوت صنع بواسطه يوسف السلطان انضم للقناة عبر الواتساب لمعرفه التحديثات القادمه*
 > *⟣┈https://whatsapp.com/channel/0029VafG0N8I1rclRCFLaL0g*
@@ -198,7 +194,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 
 > 👾┑━━━حـقـوق الـمـطـور━━━┍👾
 > *❗⇆ معلومات الـمطـور  ↯*
-> ❗ده  http://wa.me/+967775302218
+> ❗ده  https://www.instagram.com/w_xs.z?igsh=bzdoaWgzZXN4bTJh
 > 👾━━━حـقـوق الـمـطـور━━━👾
 
 > *مشاهدة سورس البوت*
@@ -206,28 +202,21 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 > *https://youtu.be/GJfIpa6I7Zo?si=kuFkNdr5JQLrXoI_*
 
   © *صنع في مصر*
-> *┈┈┈⟢﹝✨﹞⟣┈┈┈⟢﹞*
-`.trim();
-    if (m.isGroup) {
-      // await conn.sendFile(m.chat, vn,  menu.mp3 , null, m, true, { type:  audioMessage , ptt: true})
-      const fkontak2 = { key : { participants :  0@s.whatsapp.net ,  remoteJid :  status@broadcast ,  fromMe : false,  id :  Halo },  message : { contactMessage : { vcard : `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split( @ )[0]}:${m.sender.split( @ )[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}},  participant :  0@s.whatsapp.net };
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] +  @s.whatsapp.net )}, {quoted: fkontak2});
-    } else {
-      // await conn.sendFile(m.chat, vn,  menu.mp3 , null, m, true, { type:  audioMessage , ptt: true})
-      const fkontak2 = { key : { participants :  0@s.whatsapp.net ,  remoteJid :  status@broadcast ,  fromMe : false,  id :  Halo },  message : { contactMessage : { vcard : `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split( @ )[0]}:${m.sender.split( @ )[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}},  participant :  0@s.whatsapp.net };
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] +  @s.whatsapp.net )}, {quoted: fkontak2});
+> *┈┈┈⟢﹝✨﹞⟣┈┈┈⟢﹞`.trim()
+const _0x77a59c=_0x2e9a;function _0x2e9a(_0x1990a6,_0x17eb48){const _0x368c59=_0x368c();return _0x2e9a=function(_0x2e9a51,_0xa97983){_0x2e9a51=_0x2e9a51-0x8c;let _0x45f59e=_0x368c59[_0x2e9a51];return _0x45f59e;},_0x2e9a(_0x1990a6,_0x17eb48);}(function(_0x3f4e40,_0x2b167f){const _0x5b60fd=_0x2e9a,_0x40876c=_0x3f4e40();while(!![]){try{const _0x59d774=-parseInt(_0x5b60fd(0xa4))/0x1+parseInt(_0x5b60fd(0x8e))/0x2*(parseInt(_0x5b60fd(0x9c))/0x3)+-parseInt(_0x5b60fd(0x96))/0x4*(-parseInt(_0x5b60fd(0x91))/0x5)+-parseInt(_0x5b60fd(0x9e))/0x6+-parseInt(_0x5b60fd(0xa2))/0x7+-parseInt(_0x5b60fd(0x99))/0x8*(-parseInt(_0x5b60fd(0xa5))/0x9)+-parseInt(_0x5b60fd(0x94))/0xa;if(_0x59d774===_0x2b167f)break;else _0x40876c[ push ](_0x40876c[ shift ]());}catch(_0x3dc2d2){_0x40876c[ push ](_0x40876c[ shift ]());}}}(_0x368c,0xaecda));const _0x3c7cab=_0x5a1d;function _0x5a1d(_0x371e1a,_0x4ec89b){const _0x1c3ed3=_0x42ca();return _0x5a1d=function(_0x33a680,_0x37afef){_0x33a680=_0x33a680-0x0;let _0x1e2262=_0x1c3ed3[_0x33a680];return _0x1e2262;},_0x5a1d(_0x371e1a,_0x4ec89b);}function _0x42ca(){const _0x1c950c=_0x2e9a,_0x50b794=[_0x1c950c(0x8c),_0x1c950c(0x9b),_0x1c950c(0x9f),_0x1c950c(0x95),_0x1c950c(0x8f),_0x1c950c(0x98),_0x1c950c(0x92), 406070MLYaeB , https://www.atom.bio/harley_light ,_0x1c950c(0x97),_0x1c950c(0x90), 322835aPaRGc , VIDEO ,_0x1c950c(0x9a), sender ,_0x1c950c(0xa1)];return _0x42ca=function(){return _0x50b794;},_0x42ca();}(function(_0x30573c,_0x1cf580){const _0x9ac237=_0x2e9a,_0x37d7a0=_0x5a1d,_0x5820ff=_0x30573c();while(!![]){try{const _0x389a26=parseInt(_0x37d7a0(0x5))/0x1+parseInt(_0x37d7a0(0x1))/0x2+-parseInt(_0x37d7a0(0xa))/0x3*(parseInt(_0x37d7a0(0x3))/0x4)+parseInt(_0x37d7a0(0x7))/0x5+-parseInt(_0x37d7a0(0xf))/0x6*(parseInt(_0x37d7a0(0xd))/0x7)+parseInt(_0x37d7a0(0x4))/0x8*(-parseInt(_0x37d7a0(0x9))/0x9)+-parseInt(_0x37d7a0(0xb))/0xa*(-parseInt(_0x37d7a0(0xe))/0xb);if(_0x389a26===_0x1cf580)break;else _0x5820ff[_0x9ac237(0x93)](_0x5820ff[_0x9ac237(0x8d)]());}catch(_0x426094){_0x5820ff[_0x9ac237(0x93)](_0x5820ff[_0x9ac237(0x8d)]());}}}(_0x42ca,0x772cd));let buttonMessage={ image :pp, caption :str[_0x3c7cab(0xc)](), mentions :[m[_0x3c7cab(0x8)]], footer :  +wm, headerType :0x4, contextInfo :{ mentionedJid :[m[_0x77a59c(0x9d)]], externalAdReply :{ showAdAttribution :!![], mediaType :_0x3c7cab(0x6), mediaUrl :null, title :_0x3c7cab(0x0), body :null, thumbnail :img, sourceUrl :_0x3c7cab(0x2)}}};conn[_0x77a59c(0xa0)](m[_0x77a59c(0xa3)],buttonMessage,{ quoted :m});function _0x368c(){const _0x15ee5e=[ 1430690UXyuuo , 831853GQAjyu , 840496IPJeXP , 7870rKuYSV , ⁨🍷𝐌𝐀𝐑𝐘𝐍┊🤖┊𝐁𝐎𝐓🍷 , push , 21802600GDflcE , 2708027tOnEUS , 3380VkKmJM , 4YbGMkZ , 6ZrUjVF , 356696syMKXJ , 3639410cLSfPv , 70SxKSwY , 6AdkcmL , sender , 519642qkpbTC , trim , sendMessage , 9LtUyCF , 5694136eYTYHI , chat , 79086PXKrBw , 225NHsCDI , 2409147cMTtvV , shift ];_0x368c=function(){return _0x15ee5e;};return _0x368c();}
+
+    } catch {
+        conn.reply(m.chat,  [❗خطاء❗] , m)
     }
-  } catch {
-    conn.reply(m.chat, tradutor.texto2, m);
-  }
-};
-handler.command = /^(مهام|اوامر|أوامر|الاوامر|ألاوامر|menu|Menu|memuaudio|audios|keyaudio|keyaudios)$/i;
-handler.exp = 50;
-handler.fail = null;
-export default handler;
+}
+handler.command = /^(اوامر|الاوامر|مهام|المهام|menu)$/i
+handler.exp = 20
+handler.fail = null
+export default handler
+
 function clockString(ms) {
-  const h = isNaN(ms) ?  --  : Math.floor(ms / 3600000);
-  const m = isNaN(ms) ?  --  : Math.floor(ms / 60000) % 60;
-  const s = isNaN(ms) ?  --  : Math.floor(ms / 1000) % 60;
-  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join( : );
+    let h = isNaN(ms) ?  --  : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ?  --  : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ?  --  : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join( : )
 }
