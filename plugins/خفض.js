6c8a6d4305bf6368b11118c5695444e6165bd94b
Khaledst1 <166001727+Khaledst1@users.noreply.github.com>
2024-06-14 11:03:17 +0300

    Update and rename اعفاء.js to خفض.js

--- plugins/خفض.js
+++ plugins/خفض.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, participants }) => {
         await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
         
         // رابط الصورة المراد إرسالها
-        let imageUrl = 'https://telegra.ph/file/9ea7c13e92000839267ab.jpg'
+        let imageUrl = 'https://telegra.ph/file/6c26af671b9d22d85be2b.jpg'
         
         // منشن للشخص الذي قام باستعمال الأمر والشخص الذي تم خفضه من الإشراف
         let replyText = `@${m.sender.split('@')[0]} تم خفض @${user.split('@')[0]} من الإشراف✅`
@@ -23,9 +23,9 @@ let handler = async (m, { conn, participants }) => {
     }
 }
 
-handler.help = ['اعفاء @tag']
+handler.help = ['خفض @tag']
 handler.tags = ['المجموعات']
-handler.command = /^(اعفاء)$/i
+handler.command = /^(خفض)$/i
 
 handler.admin = true
 handler.group = true
