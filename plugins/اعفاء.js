3ce70c2cb4d1e662e4da1ba6f0105336f548b0d2
Khaledst1 <166001727+Khaledst1@users.noreply.github.com>
2024-06-14 10:47:16 +0300

    Update اعفاء.js

--- plugins/اعفاء.js
+++ plugins/اعفاء.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, participants }) => {
         await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
         
         // رابط الصورة المراد إرسالها
-        let imageUrl = 'https://telegra.ph/file/6c26af671b9d22d85be2b.jpg'
+        let imageUrl = 'https://telegra.ph/file/9ea7c13e92000839267ab.jpg'
         
         // منشن للشخص الذي قام باستعمال الأمر والشخص الذي تم خفضه من الإشراف
         let replyText = `@${m.sender.split('@')[0]} تم خفض @${user.split('@')[0]} من الإشراف✅`
@@ -23,9 +23,9 @@ let handler = async (m, { conn, participants }) => {
     }
 }
 
-handler.help = ['إعفاء @tag']
+handler.help = ['اعفاء @tag']
 handler.tags = ['المجموعات']
-handler.command = /^(إعفاء)$/i
+handler.command = /^(اعفاء)$/i
 
 handler.admin = true
 handler.group = true
