e55d92d61115488b8f3fca66c4565c3d065b7049
Khaledst1 <166001727+Khaledst1@users.noreply.github.com>
2024-06-26 11:26:25 +0300

    Update القاب-الاعضاء.js

--- plugins/القاب-الاعضاء.js
+++ plugins/القاب-الاعضاء.js
@@ -54,7 +54,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 *『⌘¦لمعرفة اذا اللقب متوفر』*
 > *.حذف_لقب*
 *『⌘¦لحذف اللقب المسجل』* 
-﹝❒═════﹝🍷﹞═════❒﹞*`.trim();
+*﹝❒═════﹝🍷﹞═════❒﹞*`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
