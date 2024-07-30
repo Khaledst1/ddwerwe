6e4118a72fe3cef81ae7675d751411a286636ee5
Khaledst1 <166001727+Khaledst1@users.noreply.github.com>
2024-07-30 13:53:51 +0300

    Update تصليح.js

--- plugins/تصليح.js
+++ plugins/تصليح.js
@@ -1,20 +1,31 @@
-import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync, readFileSync } from 'fs';
+import { readdirSync, unlinkSync, existsSync, promises as fs, readFileSync } from 'fs';
 import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
+  const { sender, chat, isGroup } = m;
+  const { user } = conn;
   const datas = global;
-  const idioma = datas.db.data.users[m.sender].language;
+  const idioma = datas.db.data.users[sender].language;
+  
+  // قراءة ملف الترجمة
   const _translate = JSON.parse(readFileSync(`./language/${idioma}.json`));
   const tradutor = _translate.plugins.fix_esperando_mensage;
-
-  if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: tradutor.texto1}, {quoted: m});
+  
+  // التحقق من هوية المستخدم
+  if (user.jid !== conn.user.jid) {
+    return conn.sendMessage(chat, { text: tradutor.texto1 }, { quoted: m });
   }
-  const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
+  
+  // تحديد معرفات المحادثة
+  const chatId = isGroup ? [chat, sender] : [sender];
   const sessionPath = './MysticSession/';
+  
   try {
+    // قراءة الملفات في المجلد المحدد
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
+    
+    // حذف الملفات المطابقة لمعرف المحادثة
     for (const file of files) {
       for (const id of chatId) {
         if (file.includes(id.split('@')[0])) {
@@ -24,18 +35,24 @@ const handler = async (m, { conn, usedPrefix }) => {
         }
       }
     }
+    
+    // إرسال رسالة بناءً على عدد الملفات المحذوفة
     if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: tradutor.texto2}, {quoted: m});
+      await conn.sendMessage(chat, { text: tradutor.texto2 }, { quoted: m });
     } else {
-      await conn.sendMessage(m.chat, {text: `${tradutor.texto3[0]} ${filesDeleted} ${tradutor.texto3[1]}`}, {quoted: m});
+      await conn.sendMessage(chat, { text: `${tradutor.texto3[0]} ${filesDeleted} ${tradutor.texto3[1]}` }, { quoted: m });
     }
   } catch (err) {
     console.error(tradutor.texto4, err);
-    await conn.sendMessage(m.chat, {text: tradutor.texto5}, {quoted: m});
+    await conn.sendMessage(chat, { text: tradutor.texto5 }, { quoted: m });
   }
-  await conn.sendMessage(m.chat, {text: `${tradutor.texto6} \n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+  
+  // إرسال رسالة إضافية
+  await conn.sendMessage(chat, { text: `${tradutor.texto6} \n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s` }, { quoted: m });
 };
+
 handler.help = ['fixmsgespera'];
 handler.tags = ['إصلاح'];
 handler.command = /^(صلح|ds)$/i;
+
 export default handler;
