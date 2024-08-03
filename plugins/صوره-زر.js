ddc4022549e48db8a0f806b6beb763705e330fcd
Khaledst1 <166001727+Khaledst1@users.noreply.github.com>
2024-08-03 14:47:19 +0300

    Update صوره-زر.js

--- plugins/صوره-زر.js
+++ plugins/صوره-زر.js
@@ -17,127 +17,137 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                 imageMessage: joanimiimg.imageMessage,
             },
             nativeFlowMessage: {
-                buttons: [
+  buttons: [
+   {
+  name: 'single_select',
+  buttonParamsJson: JSON.stringify({
+  title: '🍷اخـتر القـسـم🍷',
+  sections: [
+  {
+  title: 'قسم الاوامر',
+  highlight_label: 'بوت باتشيرا',
+  rows: [
+  {
+  header: 'يعطيك قسم اوامر الجروب🗣️',
+  title: 'قـسـم الجـروبـات👥✬⃝',
+  description: '',
+  id: '.قسم-الجروبات'
+  },
+  {
+  header: 'يعطيك قسم اوامر التنزيلات📤',
+  title: 'قـسـم الـتنـزيلات📥✬⃝',
+  description: '',
+  id: '.قسم-التنزيلات'
+  },
+  {
+   header: 'يعطيك قسم اوامر الترفيه🛸',
+  title: 'قـسـم الـتـرفيـه🎮✬⃝',
+  description: '',
+  id: '.قسم-الترفيه'
+  },
+  {
+    header: 'يعطيك قسم اوامر التحويل🛠️',
+  title: 'قـسـم الـتحـويل🛠️✬⃝',
+  description: '',
+  id: '.قسم-التحويل'
+  },
+  {
+  header: 'يعطيك قسم اوامر الدين والأسلام👳🏻‍♂️',
+  title: 'قـسـم الـديـني✨✬⃝',
+  description: '',
+  id: '.قسم-ديني'
+  },
+  {
+  header: 'يعطيك قسم اوامر المطور⚙️',
+  title: ' قـسـم الـمـطور🙎🏻✬⃝',
+  description: '',
+  id: '.قسم-المطور'
+  },
+   {
+    header: 'يعطيك قسم اوامر الألقاب🖊️',
+  title: ' قـسـم الألقاب📕✬⃝',
+  description: '',
+  id: '.القاب-الاعضاء'
+  },
+   {
+  header: 'يعطيك قـسم كل الاوامر🍷⃝',
+  title: 'كل-الاوامر🍷⃝',
+  description: '',
+  id: '.كل-الاوامر'
+  }
+  ]
+  }
+  ]
+  }),
+  messageParamsJson: ''
+  }, 
+  {
+                                name: "cta_url",
+                buttonParamsJson: JSON.stringify({
+                  display_text: "قنـاة الـواتـساب📣",
+                  url: "https://whatsapp.com/channel/0029VafG0N8I1rclRCFLaL0g",
+                  merchant_url: "https://whatsapp.com/channel/0029VafG0N8I1rclRCFLaL0g"
+                })
+              },
+              // Adding new button here
+              {
+                name: "cta_url",
+                buttonParamsJson: JSON.stringify({
+                  display_text: "مشاهده البوت🎦",
+                  url: "https://youtu.be/-XdmFcY3zQI?si=bzJfbQGwjUk-4rZO",
+                  merchant_url: "https://youtu.be/-XdmFcY3zQI?si=bzJfbQGwjUk-4rZO"
+                })
+              },
+              // Adding new single_select option here
+              {
+                name: 'single_select',
+                buttonParamsJson: JSON.stringify({
+                  title: '🔎معلومات البوت🔎',
+                  sections: [
                     {
-                        name: 'اختيار_واحد',
-                        buttonParamsJson: JSON.stringify({
-                            title: 'اضغط هنا',
-                            sections: [
-                                {
-                                    title: 'قائمة',
-                                    highlight_label: 'تشغيل',
-                                    rows: [
-                                        {
-                                            header: 'اختبار',
-                                            title: 'اضغط هنا',
-                                            description: 'اضغط هنا',
-                                            id: 'اختبار'
-                                        }
-                                    ]
-                                },
-                                {
-                                    highlight_label: 'تشغيل',
-                                    rows: [
-                                        {
-                                            header: 'اختبار',
-                                            title: 'اضغط هنا',
-                                            description: 'اضغط هنا',
-                                            id: 'تجربة'
-                                        }
-                                    ]
-                                },
-                                {
-                                    highlight_label: 'تشغيل',
-                                    rows: [
-                                        {
-                                            header: 'اختبار',
-                                            title: 'اضغط هنا',
-                                            description: 'اضغط هنا',
-                                            id: 'تجربة_أخرى'
-                                        }
-                                    ]
-                                }
-                            ]
-                        })
-                    },
-                    {
-                        name: 'رد_سريع',
-                        buttonParamsJson: JSON.stringify({
-                            display_text: 'رد سريع',
-                            id: 'رسالة'
-                        })
-                    },
-                    {
-                        name: 'رابط_للزيارة',
-                        buttonParamsJson: JSON.stringify({
-                            display_text: 'رابط',
-                            url: 'https://www.google.com',
-                            merchant_url: ''
-                        })
-                    },
-                    {
-                        name: 'اتصال',
-                        buttonParamsJson: JSON.stringify({
-                            display_text: 'اتصال',
-                            id: 'رسالة'
-                        })
-                    },
-                    {
-                        name: 'نسخ',
-                        buttonParamsJson: JSON.stringify({
-                            display_text: 'نسخ',
-                            id: '123456789',
-                            copy_code: 'رسالة'
-                        })
-                    },
-                    {
-                        name: 'تذكير',
-                        buttonParamsJson: JSON.stringify({
-                            display_text: 'تذكير',
-                            id: 'رسالة'
-                        })
-                    },
-                    {
-                        name: 'إلغاء_التذكير',
-                        buttonParamsJson: JSON.stringify({
-                            display_text: 'إلغاء التذكير',
-                            id: 'رسالة'
-                        })
-                    },
-                    {
-                        name: 'رسالة_عنوان',
-                        buttonParamsJson: JSON.stringify({
-                            display_text: 'رسالة عنوان',
-                            id: 'رسالة'
-                        })
-                    },
-                    {
-                        name: 'إرسال_موقع',
-                        buttonParamsJson: JSON.stringify({})
+                      title: '📜معلومات عن البوت📜',
+                      highlight_label: 'ابوهايف:♡',
+                      rows: [
+                        {
+                          header: 'صانع البوت👤',
+                          title: 'الـمطور👾',
+                          description: 'ابوهايف:♡',
+                          id: '.المطور'
+                         },
+                        {
+                         header: 'خصوصيه استخدام البوت❔❕',
+                          title: 'الاسـتخدام📜',
+                          description: '',
+                          id: '.الاستخدام'
+                        },
+                       {
+                          header: 'ابلاغ او ارسال رساله للمطور💭',
+                          title: 'طـلـب ابـلاغ📨',
+                          description: '',
+                          id: '.بلاغ'
+                        },
+                        {
+                          header: '🍷لتقييم البوت🍷',
+                          title: '♡تقييم البوت♡',
+                          description: '',
+                          id: '.تقيم'
+                        }
+                      ]
                     }
-                ],
-                messageParamsJson: {}
-            }
-        };
+                  ]
+                }),
+                messageParamsJson: ''
+              }
+            ]
+          }
+        }
+      }
+    }
+  }, {});
+}
 
-        const msg = generateWAMessageFromContent(m.chat, {
-            viewOnceMessage: {
-                message: {
-                    interactiveMessage,
-                },
-            },
-        }, { userJid: conn.user.jid, quoted: m });
-
-        msg.message.viewOnceMessage.message.interactiveMessage.contextInfo = { mentionedJid: [mentionId] };
-        conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
-
-    } else {
-        conn.sendFile(m.chat, 'JoAnimi•Error.jpg', m);      
-    }    
-};
-
-handler.help = ['صورة_زر'];
-handler.tags = ['اختبار'];
-handler.command = /^(صورة_زر)$/i;
+handler.help = ['info'];
+handler.tags = ['main'];
+handler.command = ['أوامر', 'اوامر', 'الاوامر', 'ألاوامر', 'menu', 'Menu'];
 
 export default handler;
